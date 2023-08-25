103385b9a5afd5bef0f5a6468dd083dea3dc5032
hocha113 <127948319+hocha113@users.noreply.github.com>
2023-08-25 20:57:51 +0800

    局部集合

---
+++ NPCwhoAmI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Wild.Common.AuxiliaryMeans;
+
+namespace Wild.Common.HcGlobalNPC
+{
+    public class NPCwhoAmI
+    {
+        public static int CrimsonStarBoss = -1;
+        public static int MechanicalStar = -1;
+        public static int FungusDevourer = -1;
+
+        public static List<int> FallingTwinStarsList = new List<int>();
+        public static List<int> FungusDevourerList = new List<int>();
+        public static List<int> FungusDevourerBodyList = new List<int>();
+        public static List<int> MushroomEmperorList = new List<int>();
+        public static List<int> MushroomEmperorBodyList = new List<int>();
+        public static List<int> LifeTreeList = new List<int>();
+
+        /// <summary>
+        /// 清除一次集合的-1元素
+        /// </summary>
+        public static void SweepLoadList()
+        {
+            HcMath.SweepLoadLists(ref  FallingTwinStarsList);
+        }
+        /// <summary>
+        /// 重载所有集合
+        /// </summary>
+        public static void UnLoad()
+        {
+            CrimsonStarBoss = -1;
+            MechanicalStar = -1;
+            FungusDevourer = -1;
+
+            FallingTwinStarsList = new List<int>();
+            FungusDevourerList = new List<int>();
+            FungusDevourerBodyList = new List<int>();
+            MushroomEmperorList = new List<int>();
+            MushroomEmperorBodyList = new List<int>();
+            LifeTreeList = new List<int>();
+        }
+    }
+}
---
+++ ProJwhoAmI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Wild.Common.HcGlobalProjectile
+{
+    public class ProJwhoAmI
+    {
+        public static List<int> GiantFungusTreeBombLengList = new List<int>();
+        public static List<int> GiantFungusTreeBombShortList = new List<int>();
+
+        public static void UnLoadList()
+        {
+            GiantFungusTreeBombLengList = new List<int>();
+            GiantFungusTreeBombShortList = new List<int>();
+        }
+    }
+}
